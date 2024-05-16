1d61260b4ce5b652167d4c9fbf51196eb37b5b2a
Arauz <86898345+kjarj54@users.noreply.github.com>
2024-05-15 19:05:06 -0600

    [INIT API]

--- api/src/main/java/med/voll/api/medico/DatosRegistroMedico.java
+++ api/src/main/java/med/voll/api/medico/DatosRegistroMedico.java
@@ -1,6 +1,5 @@
 package med.voll.api.medico;
 
-
 import med.voll.api.direccion.DatosDireccion;
 
 public record DatosRegistroMedico(String nombre, String email, String documento, Especialidad especialidad, DatosDireccion direccion) {
